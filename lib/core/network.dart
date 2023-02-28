c96d599997bcb05ef6d20a836039d1c264ccb9aa
kissco_wonjongseo <jswon@kissco.co.jp>
2023-02-28 10:38:50 +0900

    trigger

--- lib/core/network.dart
+++ lib/core/network.dart
@@ -20,7 +20,12 @@ class NetWork {
       'X-Naver-Client-Id': id,
       'X-Naver-Client-Secret': secretKey,
       'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      'Access-Control-Allow-Origin': false
+      "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+      "Access-Control-Allow-Credentials":
+          true, // Required for cookies, authorization headers with HTTPS
+      "Access-Control-Allow-Headers":
+          "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale",
+      "Access-Control-Allow-Methods": "POST, OPTIONS"
     };
     Map<String, dynamic> queryparameters = {
       'source': source,
